b1c19f4bad60012c4d1bb19d231947265759352a
Pablo-Correa <pablocorreacosta@gmail.com>
2022-06-25 13:01:30 -0300

    Commit 6

--- Renta1.java
+++ Renta1.java
@@ -33,4 +33,10 @@ public class Rental {
             break;
       }
     }
+
+    public int getFrequentRenterPoints() {
+      // add bonus for a two day new release rental
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+            each.getDaysRented() > 1) frequentRenterPoints ++;
+    }
 }
